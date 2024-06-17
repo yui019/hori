48b62816e432eb6a675e8a52c6eed776ee06abb1
yui019 <yui36189@gmail.com>
2024-06-17 18:32:24 +0200

    Handle default laravel migrations

--- src/Commands/InstallCommand.php
+++ src/Commands/InstallCommand.php
@@ -8,7 +8,8 @@ use Yui019\Hori\Util\StubHelper;
 
 class InstallCommand extends Command
 {
-    protected $signature = 'hori:install';
+    protected $signature = 'hori:install
+                            {--dont-delete-default-migrations : Do not delete the 3 default laravel migrations.}';
 
     public function handle()
     {
@@ -23,8 +24,21 @@ class InstallCommand extends Command
 
         $stub = new StubHelper(__DIR__ . "/../Stubs/schema.stub");
         $stub->save("database/hori/schema.php");
-        $stub->save("database/hori/.old-schema/schema.php");
+
+        $stubEmpty = new StubHelper(__DIR__ . "/../Stubs/schema_empty.stub");
+        $stubEmpty->save("database/hori/.old-schema/schema.php");
+
+        if (!$this->option("dont-delete-default-migrations")) {
+            $this->deleteDefaultMigrations();
+        }
 
         $this->info('Done!');
+    }
+
+    private function deleteDefaultMigrations()
+    {
+        File::delete("database/migrations/0001_01_01_000000_create_users_table.php");
+        File::delete("database/migrations/0001_01_01_000001_create_cache_table.php");
+        File::delete("database/migrations/0001_01_01_000002_create_jobs_table.php");
     }
 }
--- src/Schema.php
+++ src/Schema.php
@@ -19,4 +19,94 @@ class Schema
 
         array_push($this->blueprints, $blueprint);
     }
+
+    /**
+     * Create default laravel tables.
+     * These are the tables that are created by the 3 migrations that are
+     * present by default in every laravel project.
+     * This function does not, however, create the users table, because it is
+     * very common to want to modify it. Instead that table is moved to the
+     * default hori schema template.
+     */
+    public function createDefaultLaravelTables(): void
+    {
+        /// create_users_table
+        /// ==================
+
+        // $this->table('users', function (Blueprint $table) {
+        //     $table->id();
+        //     $table->string('name');
+        //     $table->string('email')->unique();
+        //     $table->timestamp('email_verified_at')->nullable();
+        //     $table->string('password');
+        //     $table->rememberToken();
+        //     $table->timestamps();
+        // });
+
+        $this->table('password_reset_tokens', function (Blueprint $table) {
+            $table->string('email')->primary();
+            $table->string('token');
+            $table->timestamp('created_at')->nullable();
+        });
+
+        $this->table('sessions', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->foreignId('user_id')->nullable()->index();
+            $table->string('ip_address', 45)->nullable();
+            $table->text('user_agent')->nullable();
+            $table->longText('payload');
+            $table->integer('last_activity')->index();
+        });
+
+        /// create_cache_table
+        /// ==================
+
+        $this->table('cache', function (blueprint $table) {
+            $table->string('key')->primary();
+            $table->mediumtext('value');
+            $table->integer('expiration');
+        });
+
+        $this->table('cache_locks', function (blueprint $table) {
+            $table->string('key')->primary();
+            $table->string('owner');
+            $table->integer('expiration');
+        });
+
+        /// create_jobs_table
+        /// ==================
+
+        $this->table('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('queue')->index();
+            $table->longText('payload');
+            $table->unsignedTinyInteger('attempts');
+            $table->unsignedInteger('reserved_at')->nullable();
+            $table->unsignedInteger('available_at');
+            $table->unsignedInteger('created_at');
+        });
+
+        $this->table('job_batches', function (Blueprint $table) {
+            $table->string('id')->primary();
+            $table->string('name');
+            $table->integer('total_jobs');
+            $table->integer('pending_jobs');
+            $table->integer('failed_jobs');
+            $table->longText('failed_job_ids');
+            $table->mediumText('options')->nullable();
+            $table->integer('cancelled_at')->nullable();
+            $table->integer('created_at');
+            $table->integer('finished_at')->nullable();
+        });
+
+        $this->table('failed_jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->text('connection');
+            $table->text('queue');
+            $table->longText('payload');
+            $table->longText('exception');
+            $table->timestamp('failed_at')->useCurrent();
+        });
+    }
 }
