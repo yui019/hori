a4e408fcefdc66ee2e5c94b23e07e56663af4c5d
yui019 <yui36189@gmail.com>
2024-06-17 02:03:44 +0200

    MVP!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    It works!!
    There's a lot of random things to add from here on.
    For example, it should reorder the tables according to the foreign
    keys (since I got some errors about that while testing).
    Also, I need to think of what to do about the default tables laravel
    creates for you. For now, I just put them in the schema and deleted
    their default migrations, but now the schema looks waaay too big and
    hard to look through because of all that noise.

--- src/Commands/GenerateCommand/GenerateCommand.php
+++ src/Commands/GenerateCommand/GenerateCommand.php
@@ -2,37 +2,47 @@
 
 namespace Yui019\Hori\Commands\GenerateCommand;
 
-use Illuminate\Console\Command;
-use Yui019\Hori\Util\StubHelper;
+use Illuminate\Console\GeneratorCommand;
+use Yui019\Hori\Commands\GenerateCommand\MigrationBodyHelper;
 
-class GenerateCommand extends Command
+class GenerateCommand extends GeneratorCommand
 {
-    protected $signature = 'hori:generate';
+    protected $signature = 'hori:generate {name}';
 
-    public function handle()
+    protected $type = "Migration";
+
+    protected function getStub()
     {
-        $this->info('Generating migrations...');
+        return realpath(__DIR__ . "/../../Stubs/migration.stub");
+    }
 
-        $schemaClass = require getcwd() . "/database/hori/schema.php";
-        $schema = new $schemaClass();
-        $schema->create();
+    protected function getPath($name)
+    {
+        // for some reason if the user inputs "test", $name will be "App\test"
+        // this puts the $name without the "App\" into $migrationName
+        $migrationName = explode("\\", $name)[1];
+
+        // 2024_06_17_123456_migrationName
+        $filename = date("Y_m_d_") . time() . "_" . $migrationName;
+
+        return getcwd() . "/database/migrations/" . $filename . ".php";
+    }
+
+    protected function replaceClass($stub, $name)
+    {
+        $newSchemaClass = require getcwd() . "/database/hori/schema.php";
+        $newSchema = new $newSchemaClass();
+        $newSchema->create();
 
         $oldSchemaClass = require getcwd() . "/database/hori/.old-schema/schema.php";
         $oldSchema = new $oldSchemaClass();
         $oldSchema->create();
 
-        // print_r($schema->blueprints);
-        print_r(SchemaCompareHelper::compare($oldSchema, $schema));
+        $migrationBodyHelper = new MigrationBodyHelper($oldSchema, $newSchema);
 
-        $stub = new StubHelper(__DIR__ . "/../../Stubs/migration.stub");
-        $migrationBodyUp = new MigrationBodyHelper("test", "create");
-        $migrationBodyDown = new MigrationBodyHelper("test", "dropIfExists");
-
-        $stub->replace("{{ up }}", $migrationBodyUp->getContent());
-        $stub->replace("{{ down }}", $migrationBodyDown->getContent());
-
-        $migrationName = "test";
-        $stub->save(getcwd() . "/database/migrations/" . $migrationName . ".php");
+        $newStub = $stub;
+        $newStub = str_replace("{{ up }}", $migrationBodyHelper->getContentUp(), $newStub);
+        $newStub = str_replace("{{ down }}", $migrationBodyHelper->getContentDown(), $newStub);
 
         // copy contents of current schema to old-schema
         file_put_contents(
@@ -40,6 +50,6 @@ class GenerateCommand extends Command
             file_get_contents(getcwd() . "/database/hori/schema.php")
         );
 
-        $this->info('Done!');
+        return $newStub;
     }
 }
--- src/Commands/GenerateCommand/MigrationBodyHelper.php
+++ src/Commands/GenerateCommand/MigrationBodyHelper.php
@@ -2,40 +2,119 @@
 
 namespace Yui019\Hori\Commands\GenerateCommand;
 
+use Illuminate\Support\Stringable;
+use Yui019\Hori\Schema;
 use Yui019\Hori\Util\StubHelper;
 
 class MigrationBodyHelper
 {
-    protected $table;
-    protected $type;
-    protected $statements;
+    protected Schema $oldSchema;
+    protected Schema $newSchema;
 
-    public function __construct(string $table, string $type)
+    public function __construct(Schema $oldSchema, Schema $newSchema)
     {
-        $this->table = $table;
-        $this->type = $type;
-        $this->statements = [];
+        $this->oldSchema = $oldSchema;
+        $this->newSchema = $newSchema;
     }
 
-    public function addStatement(): void
+    public function getContentUp(): string
     {
+        return self::getContent($this->oldSchema, $this->newSchema);
     }
 
-    public function getContent(): string
+    public function getContentDown(): string
     {
-        if ($this->type == "dropIfExists") {
-            $stub = new StubHelper(__DIR__ . "/../../Stubs/migration_body_empty.stub");
-            $stub->replace("{{ type }}", $this->type);
-            $stub->replace("{{ table }}", $this->table);
+        return self::getContent($this->newSchema, $this->oldSchema);
+    }
 
-            return $stub->getContent();
-        } else {
-            $stub = new StubHelper(__DIR__ . "/../../Stubs/migration_body.stub");
-            $stub->replace("{{ type }}", $this->type);
-            $stub->replace("{{ table }}", $this->table);
-            $stub->replace("{{ statements }}", implode("\n", $this->statements));
+    private static function getContent(Schema $oldSchema, Schema $newSchema): string
+    {
+        $operations = SchemaCompareHelper::compare($oldSchema, $newSchema);
 
-            return $stub->getContent();
+        $contents = [];
+        foreach ($operations as $operation) {
+            $content = "";
+
+            if ($operation->type == TableOperationType::Drop) {
+                $stub = new StubHelper(__DIR__ . "/../../Stubs/migration_body_empty.stub");
+                $stub->replace("{{ table }}", $operation->table);
+                $stub->replace("{{ type }}", "dropIfExists");
+
+                $content = $stub->getContent();
+            } else {
+                $stub = new StubHelper(__DIR__ . "/../../Stubs/migration_body.stub");
+                $stub->replace("{{ table }}", $operation->table);
+
+                if ($operation->type == TableOperationType::Create) {
+                    $stub->replace("{{ type }}", "create");
+                } else if ($operation->type == TableOperationType::Modify) {
+                    $stub->replace("{{ type }}", "table");
+                } else {
+                    assert(true, "Wtf this can't happen");
+                }
+
+                $stub->replace("{{ statements }}", self::getStatementsString($operation->statements));
+
+                $content = $stub->getContent();
+            }
+
+            array_push($contents, $content);
         }
+
+        return implode("\n", $contents);
+    }
+
+    /**
+     * @param TableStatement[] $statements
+     */
+    private static function getStatementsString(array $statements): string
+    {
+        $statementStrings = [];
+
+        foreach ($statements as $statement) {
+            $statementString = "\t\t\t\$table->";
+
+            if ($statement->type == TableStatementType::AddColumn) {
+                $attributes = $statement->addedColumnDefinition->getAttributes();
+                $type = $attributes["type"];
+                $name = $attributes["name"];
+
+                $params = [];
+                foreach ($attributes as $key => $value) {
+                    if ($key != "type" && $key != "name") {
+                        if (is_bool($value)) {
+                            $value = $value ? "true" : "false";
+                        }
+
+                        array_push($params, "'$key' => $value");
+                    }
+                }
+
+                $paramsString = "[ " . implode(", ", $params) . " ]";
+
+                $statementString .= "addColumn('$type', '$name', $paramsString)";
+            } else if ($statement->type == TableStatementType::AddForeignKeyConstraint) {
+                $attributes = $statement->addedForeignKeyDefinition->getAttributes();
+                $column = $attributes["columns"][0];
+                $references = $attributes["references"];
+                $on = $attributes["on"];
+
+                if ($on instanceof Stringable) {
+                    $on = $on->toString();
+                }
+
+                $statementString .= "foreign('$column')->references('$references')->on('$on')";
+            } else if ($statement->type == TableStatementType::DropColumn) {
+                $name = $statement->droppedColumnName;
+
+                $statementString .= "dropColumn('$name')";
+            }
+
+            $statementString .= ";";
+
+            array_push($statementStrings, $statementString);
+        }
+
+        return implode("\n", $statementStrings);
     }
 }
